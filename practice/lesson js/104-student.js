7e3f0d29c89790bd59bd9480d840e5b0f30e2c2f
jeylofton <jeylofton@gmail.com>
2026-07-08 21:52:28 -0500

    End of Class Updated

--- practice/lesson js/104-student.js
+++ practice/lesson js/104-student.js
@@ -6,6 +6,7 @@ function Student(valueName, valueAge, valueCourse){
 
 function registerStudent(event){
 event.preventDefault();
+
 let name = document.getElementById("studentName").value;
 let age = document.getElementById("studentAge").value;
 let course = document.getElementById("studentCourse").value;
@@ -13,6 +14,26 @@ let course = document.getElementById("studentCourse").value;
 let newStudent = new Student(name, age, course)
     
 console.log(newStudent);
+
+newInfo(newStudent);
+
+document.getElementById("studentForm").reset();
+}
+
+function newInfo(newStudent){
+    
+const tableList = document.getElementById("tableList");
+
+let row = document.createElement("tr");
+
+row.innerHTML = `
+<td> ${newStudent.name}</td>
+<td> ${newStudent.age}</td>
+<td> ${newStudent.course}</td>
+<td> <button class="btn btn-danger">Delete</button> </td>
+`;
+
+tableList.appendChild(row);
 
 
 }

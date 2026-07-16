d1b0646fb1863b6b3f651f1ec1a0d0ef4074cfcf
jeylofton <jeylofton@gmail.com>
2026-07-15 20:20:47 -0500

    Class Break Update to Code

--- scripts/services.js
+++ scripts/services.js
@@ -25,3 +25,17 @@ if (servicePrice == "") {
 }
 
 });
+
+$("#changeModeButton").click(function(){
+    $("body").toggleClass("dark-mode");
+
+    const isDark = $("body").hasClass("dark-mode");
+
+    if(isDark){
+        $("#changeModeButton").text("☀️ Change Mode");
+    }else{
+        $("#changeModeButton").text("🌙 Change Mode");
+    } 
+});
+
+
